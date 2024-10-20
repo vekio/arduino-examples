554e5525f8c9af036ac9359bfabbf2b5b31a9d18
Alberto Castañeiras <alberto@casta.me>
2024-10-20 19:34:57 +0200

    Add dht11

---
+++ dht11/dht11.ino
@@ -0,0 +1,43 @@
+#include "DHT.h"
+
+#define DHTPIN 2      // Pin where the DHT11 is connected
+#define DHTTYPE DHT11 // Specify the type of DHT sensor
+
+// Initialize the DHT11 sensor
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup()
+{
+    Serial.begin(9600);
+    Serial.println(F("DHT11 connected!"));
+
+    dht.begin(); // Start the DHT sensor
+}
+
+void loop()
+{
+    // Wait a few seconds between measurements
+    delay(2000);
+
+    float h = dht.readHumidity();    // Read humidity
+    float t = dht.readTemperature(); // Read temperature in Celsius by default
+
+    // Check if any reads failed
+    if (isnan(h) || isnan(t))
+    {
+        Serial.println(F("Failed to read from DHT sensor!"));
+        return;
+    }
+
+    // Compute the heat index in Celsius (isFahrenheit = false)
+    float hic = dht.computeHeatIndex(t, h, false);
+
+    // Print the results to the Serial Monitor
+    Serial.print(F("Humidity: "));
+    Serial.print(h);
+    Serial.print(F("%  Temperature: "));
+    Serial.print(t);
+    Serial.print(F("°C  Heat Index: "));
+    Serial.print(hic);
+    Serial.println(F("°C"));
+}
